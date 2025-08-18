3539b80a18ac3e50f07679f4188da9e09c369264
NataljaRay <ray92v@gmail.com>
2025-08-18 19:43:34 +0300

    save

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 // nuxt.config.ts
 export default defineNuxtConfig({
-  app: { baseURL: '/ray.nuxt/' },
+  // app: { baseURL: '/ray.nuxt/' },
+  // runtimeConfig: {
+  //   public: {
+  //     siteUrl: 'https://nataljaray.github.io' // твой корневой домен
+  //     // siteUrl: process.env.NUXT_PUBLIC_SITE_URL || 'http://localhost:3000'
+  //   }
+  // },
+
+  app: { baseURL: process.env.NUXT_APP_BASE_URL || '/ray.nuxt/' }, // на реальном домене поставишь '/'
   runtimeConfig: {
     public: {
-      siteUrl: 'https://nataljaray.github.io' // твой корневой домен
-      // siteUrl: process.env.NUXT_PUBLIC_SITE_URL || 'http://localhost:3000'
+      siteUrl: process.env.NUXT_PUBLIC_SITE_URL || 'http://localhost:3000'
     }
   },
 
@@ -19,9 +26,10 @@ export default defineNuxtConfig({
       preprocessorOptions: {
         scss: {
           // переменные/миксины во всех <style lang="scss">
-          additionalData: '@use "@/assets/scss/variables.scss" as *;'
-          // additionalData: '@use "@/assets/scss/variables.scss" as *;\n' +
-          //     '@use "@/assets/scss/helpers/index.scss" as *;'
+          additionalData: '@use "@/assets/scss/helpers/index" as *;'
+          // additionalData: '@use "@/assets/scss/variables.scss" as *;'
+          // additionalData: '@use "@/assets/scss/variables" as *;\n' +
+          //     '@use "@/assets/scss/helpers/index" as *;'
         }
       }
     }
