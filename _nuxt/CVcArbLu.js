0fa83f9d1c1d14928f3d6c4de48360c07143e974
Natalja Ray <natalja_fb@mail.ru>
2025-09-24 09:51:28 +0300

    Updates

--- _nuxt/CVcArbLu.js
+++
@@ -1,1 +0,0 @@
-import{c as e,o as c}from"./CH17bOXv.js";const a={__name:"music",setup(t){return(o,r)=>(c(),e("h1",null,"Music"))}};export{a as default};
---
+++ _nuxt/D5Yz68qK.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./DlAUqK2U.js";import{c,o}from"./BKBZ3R_X.js";const e={name:"contacts"};function n(s,a,r,p,_,f){return o(),c("h1",null,"contacts")}const i=t(e,[["render",n]]);export{i as default};
