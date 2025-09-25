981eeaa4f9c32608dafebfdd32b0331bce514dcd
Natalja Ray <natalja_fb@mail.ru>
2025-09-25 18:43:58 +0300

    develop-1.0: soundcloud test

--- pages/soundtracks.vue
+++ pages/soundtracks.vue
@@ -24,12 +24,23 @@
 <!--          </ClientOnly>-->
 
         <!-- Трек (миниконтролы) -->
+        <div style="width:100%;">
         <iframe
                 style="border:0; width:100%; height:120px;"
                 src="https://bandcamp.com/EmbeddedPlayer/track=731891676/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/artwork=small/transparent=true/"
                 loading="lazy"
                 seamless
         ></iframe>
+        <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay"
+                src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/soundcloud%253Atracks%253A15273221&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
+        <div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;">
+          <a href="https://soundcloud.com/foofighters" title="Foo Fighters" target="_blank"
+             style="color: #cccccc; text-decoration: none;">Foo Fighters</a> · <a
+                href="https://soundcloud.com/foofighters/foo-fighters-the-pretender"
+                title="Foo Fighters - The Pretender" target="_blank" style="color: #cccccc; text-decoration: none;">Foo
+          Fighters - The Pretender</a></div>
+
+        </div>
 
         <div class="soundtrack__media">
           <div class="soundtrack__widget">
