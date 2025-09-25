98eff1fdad4070cb69df00743189a846fed8bc48
Natalja Ray <natalja_fb@mail.ru>
2025-09-25 18:58:32 +0300

    develop-1.0: soundcloud ios test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,15 +17,15 @@ export default defineNuxtConfig({
                     content: [
                         "default-src 'self' blob: data:",
                         // "frame-src 'self' https://music.yandex.ru https://music.yandex.net https://open.spotify.com",
-                        "frame-src 'self' https://music.yandex.ru https://music.yandex.net https://open.spotify.com https://www.youtube.com https://www.youtube-nocookie.com https://vk.com https://*.vk.com https://bandcamp.com https://*.bandcamp.com https://*.soundcloud.com",
+                        "frame-src 'self' https://music.yandex.ru https://music.yandex.net https://open.spotify.com https://www.youtube.com https://www.youtube-nocookie.com https://vk.com https://*.vk.com https://bandcamp.com https://*.bandcamp.com https://w.soundcloud.com https://soundcloud.com https://*.soundcloud.com",
                         // "script-src 'self' 'unsafe-inline' 'unsafe-eval'", // нужно Nuxt/Vite HMR в dev
                         "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://vk.com https://static.vk.com https://www.youtube.com",
                         "style-src 'self' 'unsafe-inline'",
                         // "img-src 'self' data: blob:",
-                        "img-src 'self' data: blob: https://vk.com https://*.vk.com https://avatars.yandex.net https://i.ytimg.com https://i.scdn.co",
+                        "img-src 'self' data: blob: https://vk.com https://*.vk.com https://avatars.yandex.net https://i.ytimg.com https://i.scdn.co https://i1.sndcdn.com https://i.sndcdn.com https://a-v2.sndcdn.com",
                         "font-src 'self' data:",
                         // "connect-src 'self' ws:",
-                        "connect-src 'self' ws: https://vk.com https://api.vk.com https://bandcamp.com https://*.bandcamp.com https://*.soundcloud.com",
+                        "connect-src 'self' ws: https://vk.com https://api.vk.com https://bandcamp.com https://*.bandcamp.com https://w.soundcloud.com https://soundcloud.com https://*.soundcloud.com",
                         // "connect-src 'self' ws: https://ipapi.co",
                         isProd ? "upgrade-insecure-requests" : null,        // ← только в проде
                     ].filter(Boolean).join('; ')
--- pages/soundtracks.vue
+++ pages/soundtracks.vue
@@ -31,14 +31,24 @@
                 loading="lazy"
                 seamless
         ></iframe>
-        <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay"
-                src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/soundcloud%253Atracks%253A15273221&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
-        <div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;">
-          <a href="https://soundcloud.com/foofighters" title="Foo Fighters" target="_blank"
-             style="color: #cccccc; text-decoration: none;">Foo Fighters</a> · <a
-                href="https://soundcloud.com/foofighters/foo-fighters-the-pretender"
-                title="Foo Fighters - The Pretender" target="_blank" style="color: #cccccc; text-decoration: none;">Foo
-          Fighters - The Pretender</a></div>
+<!--        <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay"-->
+<!--                src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/soundcloud%253Atracks%253A15273221&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>-->
+
+          <iframe
+                  width="100%"
+                  height="166"
+                  scrolling="no"
+                  frameborder="no"
+                  src="https://w.soundcloud.com/player/?url=https%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F15273221&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true">
+          </iframe>
+
+
+          <!--        <div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;">-->
+<!--          <a href="https://soundcloud.com/foofighters" title="Foo Fighters" target="_blank"-->
+<!--             style="color: #cccccc; text-decoration: none;">Foo Fighters</a> · <a-->
+<!--                href="https://soundcloud.com/foofighters/foo-fighters-the-pretender"-->
+<!--                title="Foo Fighters - The Pretender" target="_blank" style="color: #cccccc; text-decoration: none;">Foo-->
+<!--          Fighters - The Pretender</a></div>-->
 
         </div>
 
