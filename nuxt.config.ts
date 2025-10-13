ac327e10e6490cfff5b11f39586d1bd53edff113
NataljaRay <ray92v@gmail.com>
2025-10-13 17:14:20 +0300

    main

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 import { defineNuxtConfig } from 'nuxt/config'
 
 const isProd = process.env.NODE_ENV === 'production'
-const baseURL = process.env.NUXT_APP_BASE_URL || '/ray.nuxt/'
+const baseURL = process.env.NUXT_APP_BASE_URL || '/'
 
 export default defineNuxtConfig({
     app: {
