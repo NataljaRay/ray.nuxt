1f4089b2ab88d517813116a26460ca6e770079b3
NataljaRay <ray92v@gmail.com>
2025-10-18 16:21:09 +0300

    release-2: test img 215

--- components/blocks/AlbumMin.vue
+++ components/blocks/AlbumMin.vue
@@ -5,7 +5,7 @@
               <img :src="(isMobile || isTablet) && props.compressed ? imgUrlSm(album.cover) : imgUrl(album.cover)"
                    :alt="album.title"
                    :title="album.title || ''"
-                   width="342" height="342" decoding="async"
+                   width="309" height="309" decoding="async"
                    fetchpriority="high"
               >
 <!--              </ClientOnly>-->
