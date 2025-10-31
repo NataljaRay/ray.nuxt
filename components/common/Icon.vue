8acc1ac9c7fc2900195519e9b96a67b1cb0fcb82
NataljaRay <ray92v@gmail.com>
2025-10-31 12:34:32 +0300

    release-2: fix buy links, add tg book

--- components/common/Icon.vue
+++ components/common/Icon.vue
@@ -9,6 +9,7 @@
     import IconDownload from '@/assets/icons/download.svg?raw'
     import IconLink from '@/assets/icons/socials/link.svg?raw'
     import IconTg from '@/assets/icons/socials/tg.svg?raw'
+    import IconTgBig from '@/assets/icons/socials/tgBig.svg?raw'
     import IconVk from '@/assets/icons/socials/vk.svg?raw'
     import ExtLink from '@/assets/icons/ext-link.svg?raw'
     // import IconYm from '@/assets/icons/music/ym.svg?raw'
@@ -22,6 +23,7 @@
     const icons = {
         'vk': IconVk,
         'tg': IconTg,
+        'tgBig': IconTgBig,
         'link': IconLink,
         'download': IconDownload,
         'record': IconRecord,
